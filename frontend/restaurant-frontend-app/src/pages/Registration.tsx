9492ec8ffc25739e02344e6be3ca4df484049598
Temirlan <temadjumanov@gmail.com>
2025-03-11 17:29:08 +0600

    init: installed libraries (tailwindcss, shadcn, react-router), configured styles, created base template

---
+++ frontend/restaurant-frontend-app/src/pages/Registration.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Registration = () => {
+  return (
+    <div className="flex">
+      <div>
+        <h1 className=" text-9xl">Hello</h1>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Registration;
