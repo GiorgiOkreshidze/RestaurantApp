76422fd455843d74b183c0b8184a081212f90d15
Giorgi Okreshidze <okreshidze.giorgi@kiu.edu.ge>
2025-03-14 23:00:43 +0400

    feat: Create Locations Endpoint for Main Page
    Add locations endpoint and DynamoDB integration
    
    Updated deployment_resources.json to add permissions for DynamoDB and a new `/locations` endpoint with CORS support. Introduced `GetLocationsAction` to handle GET requests and return location data. Added a new `Location` model and updated `DynamoDBService` to include a method for fetching locations. Modified `ApiHandler` to route requests to the new action and updated `IDynamoDBService` interface accordingly. Configured environment variable for the locations DynamoDB table in lambda_config.json.

--- backend/restaurant-backend-app/dnapp/lambdas/restaurant-api-handler/Services/DynamoDBService.cs
+++ backend/restaurant-backend-app/dnapp/lambdas/restaurant-api-handler/Services/DynamoDBService.cs
@@ -1,6 +1,9 @@
-﻿using Amazon.DynamoDBv2;
+﻿using Amazon.CognitoIdentityProvider.Model;
+using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.DynamoDBv2.Model;
+using Function.Models;
 using Function.Services.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -16,6 +19,7 @@ namespace Function.Services
         private readonly AmazonDynamoDBClient _dynamoDBClient;
         private readonly string? _waitersTableName = Environment.GetEnvironmentVariable("DYNAMODB_WAITERS_TABLE_NAME");
         private readonly string? _emailIndexName = Environment.GetEnvironmentVariable("WAITERS_TABLE_EMAIL_INDEX_NAME");
+        private readonly string? _locationsTableName = Environment.GetEnvironmentVariable("DYNAMODB_LOCATIONS_TABLE_NAME");
 
         public DynamoDBService()
         {
@@ -39,6 +43,41 @@ namespace Function.Services
 
             var response = await _dynamoDBClient.QueryAsync(request);
             return response.Count > 0;
+        }
+
+        public async Task<List<Location>> GetListOfLocations()
+        {
+            var documentList = await ScanDynamoDBTableAsync(_locationsTableName);
+            return MapDocumentsToLocations(documentList);
+        }
+
+        private List<Location> MapDocumentsToLocations(List<Document> documentList)
+        {
+            return documentList.Select(doc => new Location
+            {
+                Id = doc.TryGetValue("id", out var id) ? id : "",
+                Address = doc.TryGetValue("address", out var address) ? address : "",
+                Description = doc.TryGetValue("description", out var description) ? description : "",
+                TotalCapacity = doc.TryGetValue("totalCapacity", out var totalCapacity) ? totalCapacity : "",
+                AverageOccupancy = doc.TryGetValue("averageOccupancy", out var averageOccupancy) ? averageOccupancy : "",
+                ImageUrl = doc.TryGetValue("imageUrl", out var imageUrl) ? imageUrl : "",
+                Rating = doc.TryGetValue("rating", out var rating) ? rating : ""
+            }).ToList();
+        }
+
+        private async Task<List<Document>> ScanDynamoDBTableAsync(string? tableName)
+        {
+            var table = Table.LoadTable(_dynamoDBClient, tableName);
+            var scanFilter = new ScanFilter();
+            var search = table.Scan(scanFilter);
+
+            List<Document> documentList = new List<Document>();
+            do
+            {
+                documentList.AddRange(await search.GetNextSetAsync());
+            } while (!search.IsDone);
+
+            return documentList;
         }
     }
 }
