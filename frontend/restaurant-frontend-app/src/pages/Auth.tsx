f3237b17c88181582f469bbb27562cb6a9ce3fe5
Roman Zvarych <roman_zvarych@epam.com>
2025-03-17 22:31:31 +0200

    refactor: small fixes

--- frontend/restaurant-frontend-app/src/pages/Auth.tsx
+++ frontend/restaurant-frontend-app/src/pages/Auth.tsx
@@ -6,7 +6,7 @@ export const Auth = () => {
   const { pathname } = useLocation();
 
   return (
-    <Container>
+    <Container className="flex *:basis-full items-center min-h-[100vh]">
       <div className="flex flex-col gap-[32px] md:flex-row-reverse">
         <section className="flex items-center justify-center gap-[12px] md:flex-1/2 md:flex-col-reverse md:gap-[72px]">
           <Logo className="w-[48px] md:w-[min(70%,417px)]" />
