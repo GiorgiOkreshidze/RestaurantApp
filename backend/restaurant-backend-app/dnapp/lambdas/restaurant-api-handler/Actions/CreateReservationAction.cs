739a374ed149983a71ee4ea6cbb0c5ffb14e1924
DarjaLeonova <darja.leonova.it@gmail.com>
2025-03-18 16:09:28 +0200

    feat: create /api/reservations/client endpoint. currently there are 'upsert' functionality

---
+++ backend/restaurant-backend-app/dnapp/lambdas/restaurant-api-handler/Actions/CreateReservationAction.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Function.Models;
+using Function.Services.Interfaces;
+using SimpleLambdaFunction.Actions;
+using SimpleLambdaFunction.Services;
+using SimpleLambdaFunction.Services.Interfaces;
+
+namespace Function.Services;
+
+public class CreateReservationAction
+{
+    private readonly IDynamoDBService _dynamoDBService;
+    private readonly IAuthenticationService _authenticationService;
+
+    public CreateReservationAction()
+    {
+        _dynamoDBService = new DynamoDBService();
+        _authenticationService = new AuthenticationService();
+    }
+
+    public async Task<APIGatewayProxyResponse> CreateReservation(APIGatewayProxyRequest request)
+    {
+        var accessToken = ActionUtils.GetAccessToken(request);
+
+        try
+        {
+            var userInfo = await _authenticationService.GetUserDetailsAsync(accessToken);
+            var reservationRequest = JsonSerializer.Deserialize<ReservationRequest>(request.Body);
+
+            if (reservationRequest == null)
+            {
+                throw new ArgumentException("Reservation request body was null");
+            }
+            
+            var reservationId = Guid.NewGuid().ToString();
+            if (reservationRequest.Id != null)
+            {
+                reservationId = reservationRequest.Id;
+            }
+
+            var location = await _dynamoDBService.GetLocationById(reservationRequest.LocationId);
+
+            var firstName = userInfo.GetValueOrDefault("given_name");
+            var lastName = userInfo.GetValueOrDefault("family_name");
+
+            var reservation = new Reservation
+            {
+                Id = reservationId,
+                Date = reservationRequest.Date,
+                FeedbackId = "NOT IMPLEMENTED YET AND ISN'T REQUIRED",
+                GuestsNumber = reservationRequest.GuestsNumber,
+                LocationAddress = location.Address,
+                PreOrder = "NOT IMPLEMENTED YET",
+                Status = Status.Reserved.ToString(),
+                TableNumber = reservationRequest.TableNumber,
+                TimeFrom = reservationRequest.TimeFrom ,
+                TimeTo = reservationRequest.TimeTo,
+                TimeSlot = reservationRequest.TimeFrom + " - " + reservationRequest.TimeTo,
+                UserInfo = firstName + " " + lastName,
+            };
+
+            var reservationResponse = await _dynamoDBService.UpsertReservation(reservation);
+            
+            return ActionUtils.FormatResponse(200, reservationResponse);
+        }
+        catch (JsonException e)
+        {
+            return ActionUtils.FormatResponse(400, new
+            {
+                message = "The json was in invalid format"
+            });
+        }
+        catch (Exception e)
+        {
+            return ActionUtils.FormatResponse(500, new
+            {
+                message = e.Message
+            });
+        }
+    }
+}
