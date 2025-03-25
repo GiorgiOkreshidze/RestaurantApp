a836e085e518bbe2e0c013303130d1be234d900e
DarjaLeonova <darja.leonova.it@gmail.com>
2025-03-25 16:11:38 +0200

    feat: new 'waiter' repository

---
+++ backend/restaurant-backend-app/dnapp/lambdas/restaurant-api-handler/Repository/WaiterRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Function.Mappers;
+using Function.Models.User;
+using Function.Repository.Interfaces;
+
+namespace Function.Repository;
+
+public class WaiterRepository : IWaiterRepository
+{
+    private readonly AmazonDynamoDBClient _dynamoDBClient;
+    private readonly string? _usersTableName = Environment.GetEnvironmentVariable("DYNAMODB_USERS_TABLE_NAME");
+    
+    public WaiterRepository()
+    {
+        _dynamoDBClient = new AmazonDynamoDBClient();
+    }
+    
+    public async Task<List<User>> GetWaitersByLocationAsync(string locationId)
+    {
+        var documentList = await DynamoDbUtils.ScanDynamoDbTableAsync(_dynamoDBClient, _usersTableName);
+        var filteredDocuments = documentList
+            .Where(doc =>
+                doc.TryGetValue("locationId", out var docLocationId) &&
+                docLocationId.ToString() == locationId &&
+                doc.TryGetValue("role", out var role) &&
+                role.ToString() == "Waiter") // Filter for waiters only
+            .ToList();
+
+        return Mapper.MapDocumentsToUsers(filteredDocuments);
+    }
+}
