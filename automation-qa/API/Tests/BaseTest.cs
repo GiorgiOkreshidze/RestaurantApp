dd5c8107fea0b5f44b216b0423b7fc5e08523083
Irina Khrol <irina_khrol@epam.com>
2025-03-27 13:44:52 +0200

    feat: init base page and base test

---
+++ automation-qa/API/Tests/BaseTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using automation_qa.Framework;
+
+namespace ApiTests
+{
+    public class BaseTest
+    {
+        protected RestClient _client;
+
+        [SetUp]
+        public void Setup()
+        {
+            _client = new RestClient(BaseConfiguration.ApiBaseUrl);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _client?.Dispose();
+        }
+    }
+}
