d382e5caa5edc7f194292fe03b1add3d6da60aa4
Irina Khrol <irina_khrol@epam.com>
2025-03-27 13:54:03 +0200

    feat: set up WebDriver and testing framework

---
+++ automation-qa/Framework/BaseConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace automation_qa.Framework
+{
+    public class BaseConfiguration
+    {
+        public static string ApiBaseUrl { get; set; } = "https://apc79c27sb.execute-api.eu-west-2.amazonaws.com/api";
+        public static int DefaultTimeout { get; set; } = 10;
+        public static string DefaultBrowser { get; set; } = "chrome";
+    }
+}
