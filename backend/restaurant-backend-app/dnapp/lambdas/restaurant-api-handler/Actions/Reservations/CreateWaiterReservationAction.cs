c3ecd8a8b8209ae110a2ad82391b1aa9cb7e185a
DarjaLeonova <darja.leonova.it@gmail.com>
2025-03-30 14:39:21 +0300

    feat: extract the common validation logic

--- backend/restaurant-backend-app/dnapp/lambdas/restaurant-api-handler/Actions/Reservations/CreateWaiterReservationAction.cs
+++ backend/restaurant-backend-app/dnapp/lambdas/restaurant-api-handler/Actions/Reservations/CreateWaiterReservationAction.cs
@@ -1,37 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Amazon.CognitoIdentityProvider.Model;
 using Amazon.Lambda.APIGatewayEvents;
 using Function.Actions.Validators;
 using Function.Models.Requests;
+using Function.Models.User;
 using Function.Services;
 using Function.Services.Interfaces;
 
 namespace Function.Actions.Reservations;
 
-public class CreateReservationAction
+public class CreateWaiterReservationAction
 {
     private readonly IAuthenticationService _authenticationService;
     private readonly IReservationService _reservationService;
     private readonly IUserService _userService;
 
-    public CreateReservationAction()
+    public CreateWaiterReservationAction()
     {
         _authenticationService = new AuthenticationService();
         _reservationService = new ReservationService();
         _userService = new UserService();
     }
-
-    public async Task<APIGatewayProxyResponse> CreateReservationAsync(APIGatewayProxyRequest request)
+    
+public async Task<APIGatewayProxyResponse> CreateReservationAsync(APIGatewayProxyRequest request)
     {
         var accessToken = ActionUtils.GetAccessToken(request);
         var userInfo = await _authenticationService.GetUserDetailsAsync(accessToken);
-        var reservationRequest = JsonSerializer.Deserialize<ClientReservationRequest>(request.Body);
+        var reservationRequest = JsonSerializer.Deserialize<WaiterReservationRequest>(request.Body);
         var email = userInfo.GetValueOrDefault("email");
-        
+
         if (reservationRequest == null)
         {
             throw new ArgumentException("Reservation request body was null");
@@ -41,8 +43,25 @@ public class CreateReservationAction
         {
             throw new UnauthorizedException("User is not registered");
         }
-        
+
         var user = await _userService.GetUserByEmailAsync(email);
+        
+        if (user.Role != Roles.Waiter)
+        {
+            throw new UnauthorizedException("Only waiters can create or modify waiter reservations");
+        }
+
+   
+        // Create operation
+        if (!new[] { "CUSTOMER", "VISITOR" }.Contains(reservationRequest.ClientType))
+        {
+            throw new ArgumentException("ClientType must be either CUSTOMER or VISITOR");
+        }
+
+        if (reservationRequest.ClientType == "CUSTOMER" && string.IsNullOrEmpty(reservationRequest.CustomerName))
+        {
+            throw new ArgumentException("CustomerName is required for CUSTOMER type reservations");
+        }
 
         ReservationValidator.ValidateGuestsNumber(reservationRequest.GuestsNumber);
         
@@ -51,12 +70,11 @@ public class CreateReservationAction
         var parsedEndTime = ReservationValidator.ParseTime(reservationRequest.TimeTo, "TimeTo");
         var reservationDateTimeFrom = parsedDate.Add(parsedStartTime);
         var reservationDateTimeTo = parsedDate.Add(parsedEndTime);
-        
+
         ReservationValidator.ValidateFutureDateTime(reservationDateTimeFrom);
         ReservationValidator.ValidateFutureDateTime(reservationDateTimeTo);
-        
-        var reservationResponse = await _reservationService.UpsertReservationAsync(reservationRequest, user);
 
+        var reservationResponse = await _reservationService.UpsertReservationAsync(reservationRequest, user);
         return ActionUtils.FormatResponse(200, reservationResponse);
     }
 }
