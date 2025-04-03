fe5b030314ccb97a7b5d45f0c351ede40c2bc49b
Roman Zvarych (hisbvdis) <hisbvdis@gmail.com>
2025-04-03 17:28:41 +0300

    fix

--- frontend/restaurant-frontend-app/src/pages/ClientReservations.tsx
+++ frontend/restaurant-frontend-app/src/pages/ClientReservations.tsx
@@ -32,7 +32,7 @@ export const ClientReservations = () => {
         {reservationsLoading ? (
           <Spinner />
         ) : reservations?.length > 0 ? (
-          <div className="grow-1 content-start grid gap-[2rem] lg:grid-cols-[repeat(auto-fit,minmax(350px,1fr))]">
+          <div className="grow-1 content-start grid gap-[2rem] lg:grid-cols-[repeat(auto-fill,minmax(350px,1fr))]">
             {reservations.map((reservation) => (
               <ReservationCard key={reservation.id} reservation={reservation} />
             ))}
