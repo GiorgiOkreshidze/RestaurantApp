ca6754634e021e06b4694934dd924183d87a75f9
Irina Khrol <irina_khrol@epam.com>
2025-04-04 21:37:00 +0300

    add changes

--- automation-qa/Framework/BaseConfiguration.cs
+++ automation-qa/Framework/BaseConfiguration.cs
@@ -2,8 +2,8 @@
 {
     public class BaseConfiguration
     {
-        public static string ApiBaseUrl { get; set; } = "https://aazay6kit4.execute-api.eu-west-2.amazonaws.com/api";
-        public static string UiBaseUrl { get; set; } = "http://localhost:5173/";
+        public static string ApiBaseUrl { get; set; } = "https://xzi78ndtre.execute-api.eu-west-2.amazonaws.com/api";
+        public static string UiBaseUrl { get; set; } = "http://localhost:5174/";
         public static int DefaultTimeout { get; set; } = 10;
         public static string DefaultBrowser { get; set; } = "chrome";
     }
