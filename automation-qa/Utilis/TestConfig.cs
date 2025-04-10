62ccf73e85731b654dd850789628d0373fc0b700
Irina Khrol <irina_khrol@epam.com>
2025-04-10 14:34:37 +0300

    feat: new test config to avoid hardcoding in testing

--- automation-qa/Utilis/TestConfig.cs
+++ automation-qa/Utilis/TestConfig.cs
@@ -11,8 +11,9 @@ namespace ApiTests.Utilities
         public string AdminUserPassword { get; set; }
         public string ValidLocationId { get; set; }
         public string ValidReservationId { get; set; }
+        public string WaiterEmail { get; set; }
+        public string WaiterPassword { get; set; }
 
-        // Singleton instance
         private static TestConfig _instance;
         public static TestConfig Instance
         {
@@ -30,7 +31,6 @@ namespace ApiTests.Utilities
         {
             string configFile = "testConfig.json";
 
-            // Альтернативный путь, если файл не найден
             if (!File.Exists(configFile))
             {
                 configFile = Path.Combine(Directory.GetCurrentDirectory(), "testConfig.json");
@@ -42,7 +42,6 @@ namespace ApiTests.Utilities
                 return JsonConvert.DeserializeObject<TestConfig>(json);
             }
 
-            // Если файл не найден, возвращаем конфигурацию по умолчанию
             return new TestConfig
             {
                 TestUserEmail = "test@example.com",
@@ -50,7 +49,9 @@ namespace ApiTests.Utilities
                 AdminUserEmail = "admin@example.com",
                 AdminUserPassword = "AdminP@ss123!",
                 ValidLocationId = "8c4fc44e-c1a5-42eb-9912-55aeb5111a99",
-                ValidReservationId = "43a479ef-4aa0-4472-b72e-7833f90a591"
+                ValidReservationId = "43a479ef-4aa0-4472-b72e-7833f90a591",
+                WaiterEmail = "waiter@restaurant.com",
+                WaiterPassword = "WaiterP@ss123!"
             };
         }
     }
